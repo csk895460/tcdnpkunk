077bb15e9c0d8ed518e3519a9c1301604c7e1ad5
Aman-sir-english <amansirenglish@outlook.com>
2024-06-25 13:44:20 +0530

    Initial commit

---
+++ modules/config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_NAME = "kunalupdated"
+BOT_TOKEN = "7197700654:AAFXpqfG9T45jsxftHJGvmOZLnyCa5qyZ6c"
+API_ID = 20299588
+API_HASH = "f550d6179131c293d658f15f8c24f594"
+MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
