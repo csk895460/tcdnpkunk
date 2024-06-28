aa580facf47e186f3fb1fd8c805f03303b8723df
Aman-sir-english <amansirenglish@outlook.com>
2024-06-28 15:47:09 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "jokerbhaisahab1"
-BOT_TOKEN = "7398977714:AAEZ_ubtCLgBKeRcb4WO4H6FX3JiBYY8GTE"
+BOT_NAME = "jokerbhaisahab2"
+BOT_TOKEN = "7305319947:AAH-AXj92-9ZkqJghC4Ggpw7Gj0J0uZ-iCg"
 API_ID = 20299588
 API_HASH = "f550d6179131c293d658f15f8c24f594"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
