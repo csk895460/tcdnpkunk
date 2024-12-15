f7466ac93a278c145f0b40662cafd1e5013b1c4d
csk895460 <159976535+csk895460@users.noreply.github.com>
2024-12-15 08:46:00 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "jokerbhaisahab3"
+BOT_NAME = "Autoca344p_bot"
 BOT_TOKEN = "7326006111:AAE-8BcoQkmyBTkqfkD2Wjc0mj1LAVKf6Gc"
-API_ID = 20299588
-API_HASH = "f550d6179131c293d658f15f8c24f594"
+API_ID = 25038096
+API_HASH = "098112aae38be62db58363267a061b59"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
