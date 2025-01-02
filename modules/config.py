be93a3abdd1b58d825e8c9aa4f9f836874f2bbf6
Csk <159976535+csk895460@users.noreply.github.com>
2025-01-02 10:34:22 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "Swksgwgyw724_2254_bot"
-BOT_TOKEN = "7719688210:AAEk3oQMJMhIVFE_8OnURYcGM9XtiBdMOOQ"
+BOT_NAME = "tfffr_bot"
+BOT_TOKEN = "7719688210:hghhfff"
 API_ID = 25038096
 API_HASH = "098112aae38be62db58363267a061b59"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
