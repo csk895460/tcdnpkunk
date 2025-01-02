8d44c8d0be6f7fed11a72101e9089bf589ea7f40
Csk <159976535+csk895460@users.noreply.github.com>
2025-01-02 10:24:17 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "losjs_bot"
-BOT_TOKEN = "7642805832:AAFISsNrEqRWx7Vz6TyjkvjAc"
+BOT_NAME = "Cfgdwra23_473_t_bot"
+BOT_TOKEN = "8027464304:AAGNwKw-BbsZMTFcGXfMMOZ2OhmXQYIVIhw"
 API_ID = 25038096
 API_HASH = "098112aae38be62db58363267a061b59"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
