0e54275f80284f5a662cee74f92c29894d7dd6d2
Csk <159976535+csk895460@users.noreply.github.com>
2025-01-02 10:19:15 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "Autoca344p_bot"
-BOT_TOKEN = "7642805832:AAFISsNrEqRWx7Vz6bip42rhlNrTyjkvjAc"
+BOT_NAME = "losjs_bot"
+BOT_TOKEN = "7642805832:AAFISsNrEqRWx7Vz6TyjkvjAc"
 API_ID = 25038096
 API_HASH = "098112aae38be62db58363267a061b59"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
